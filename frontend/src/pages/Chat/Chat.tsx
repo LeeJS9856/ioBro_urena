22dc3902d8f87fae8b6ae3503e054d8b25714386
LeeJS9856 <wnstj1057@naver.com>
2025-01-02 18:15:07 +0900

    Fix: 뒤로가기 버튼 기능하도록 수정

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import TopNavigation from '../../components/Navigation/TopNavigation';
 import QuestionBalloon from '../../components/Balloons/QuestionBalloon';
 import AnswerBalloon from '../../components/Balloons/AnswerBalloon';
@@ -14,16 +15,26 @@ interface QnA {
 const Chat: React.FC = () => {
   const [question, setQuestion] = useState('');
   const [sampleQnA, setSampleQnA] = useState([{}]);
+  const navigation = useNavigation();
+
   const handleQuestion = () => {
     setSampleQnA(prevQnA => [...prevQnA, {Q: question}]);
     setSampleQnA(prevQnA => [...prevQnA, {A: '답변입니다.'}]);
     setQuestion('');
   };
 
+  const handleGoBack = () => {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+  } else {
+      navigation.navigate('Main' as never);
+  }
+  };
+
 
   return (
     <SafeAreaView style={Styles.background}>
-      <TopNavigation styleType={2}/>
+      <TopNavigation styleType={2} onBackPress={handleGoBack}/>
       <ScrollView>
         <View style={Styles.startContainer}>
           {sampleQnA.map((item: QnA, index)=> {
