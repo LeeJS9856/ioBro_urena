482f4cc709e68d63bbffb253b4d3cd9328314521
LeeJS9856 <wnstj1057@naver.com>
2025-02-25 12:59:03 +0900

    Feat : 복사, 추천, 비추천 아이콘 추가

--- frontend/src/components/Balloons/AnswerBalloon.tsx
+++ frontend/src/components/Balloons/AnswerBalloon.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { TextGmarketSans } from '../../utils/CustomText';
 import { ActivityIndicator } from 'react-native-paper';
 import Markdown from 'react-native-markdown-display';
+import Icon from 'react-native-vector-icons/Feather';
 
 interface AnswerBalloonProps {
   text: string;
@@ -10,23 +11,55 @@ interface AnswerBalloonProps {
 }
 
 const AnswerBalloon: React.FC<AnswerBalloonProps> = ({ text, loading }) => {
-  return (
-    <View style={styles.AnswerContainer}>
-        <View style={styles.Icon}>
-            <TextGmarketSans style={styles.IconText}>U</TextGmarketSans>
+    const [selectedFeedback, setSelectedFeedback] = useState<string | null>(null);
+    const handleFeedback = (type: string) => {
+        setSelectedFeedback((prev) => (prev === type ? null : type));
+    };
+    return (
+        <View style={styles.AnswerContainer}>
+            <View style={styles.urenaIcon}>
+                <TextGmarketSans style={styles.IconText}>U</TextGmarketSans>
+            </View>
+            <View style={styles.AnswerBallon}>
+                {loading ? (
+                <ActivityIndicator
+                    animating={true}
+                    size="small"
+                    color="#714DF5" />
+                ) : (
+                <>
+                    <Markdown style={markdownStyles}>{text}</Markdown>
+                    <View style={styles.feedback}>
+                        {selectedFeedback === null && (
+                            <>
+                            <TouchableOpacity>
+                                <Icon name="copy" size={20}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleFeedback('thumbs-up')}>
+                                <Icon name="thumbs-up" size={20}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleFeedback('thumbs-down')}>
+                                <Icon name="thumbs-down" size={20}/>
+                            </TouchableOpacity>
+                            </>
+                        )}
+                        {selectedFeedback !== null && (
+                            <>
+                                <TouchableOpacity>
+                                    <Icon name="copy" size={20}/>
+                                </TouchableOpacity>
+                                <Icon
+                                    name={selectedFeedback}
+                                    size={20}
+                                    />
+                            </>
+                        )}
+                    </View>
+                </>
+                )}
+            </View>
         </View>
-        <View style={styles.AnswerBallon}>
-            {loading ? (
-            <ActivityIndicator
-                animating={true}
-                size="small"
-                color="#714DF5" />
-            ) : (
-            <Markdown style={markdownStyles}>{text}</Markdown>
-            )}
-        </View>
-    </View>
-  );
+    );
 };
 
 const markdownStyles = StyleSheet.create({
@@ -48,7 +81,7 @@ const styles = StyleSheet.create({
     AnswerBallon: {
         flex: 1,
     },
-    Icon: {
+    urenaIcon: {
         width: 30,
         height: 30,
         borderRadius: 15,
@@ -64,6 +97,15 @@ const styles = StyleSheet.create({
         lineHeight: 16,
         color: '#714DF5',
         fontSize: 16,
+    },
+    feedback: {
+        flexDirection: 'row',
+        marginTop: 8,
+        paddingHorizontal: 13,
+        gap: 19,
+    },
+    Icon: {
+        color: '#464A4D',
     },
 });
 
